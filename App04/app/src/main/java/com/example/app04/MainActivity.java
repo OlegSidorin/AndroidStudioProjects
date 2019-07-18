bb14bfb9ef05b3829e215a96f496d2dfc04efa5d
Oleg Sidorin <oleg.sidorin@gmail.com>
2019-07-18 17:45:36 +0300

    update to 18.07

--- App04/app/src/main/java/com/example/app04/MainActivity.java
+++ App04/app/src/main/java/com/example/app04/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     static final String KEY_NAME = "name user";
     Button myBtnShowName;
     Button myBtnOk;
-    TextView myTxtView01;
+    TextView myTxtView01, myTxtView02;
     EditText myEditText;
     String name;
     int anInt = 0;
@@ -27,9 +27,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myBtnOk = findViewById(R.id.btn_ok);
-        myEditText = findViewById(R.id.editText);
+        myEditText = findViewById(R.id.myEditText01);
         myBtnShowName = findViewById(R.id.btn_showname);
         myTxtView01 = findViewById(R.id.myTextView01);
+        myTxtView02 = findViewById(R.id.myTextView02);
         Log.d("tag","Вызван метод onCreate" );
 
         findViewById(R.id.myButtonPush).setOnClickListener(new View.OnClickListener() {
@@ -58,6 +59,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void change_in_myTextView02(String str){
+        myTxtView02.append(str);
+    }
+
+    public void push_1 (View veiw){
+        change_in_myTextView02("1");
+    }
+    public void push_2 (View veiw){
+        change_in_myTextView02("2");
+    }
+    public void push_3 (View veiw){
+        change_in_myTextView02("3");
+    }
+    public void push_4 (View veiw){
+        change_in_myTextView02("4");
+    }
+    public void push_5 (View veiw){
+        change_in_myTextView02("5");
+    }
+    public void push_6 (View veiw){
+        change_in_myTextView02("6");
+    }
+    public void push_7 (View veiw){
+        change_in_myTextView02("7");
+    }
+    public void push_8 (View veiw){
+        change_in_myTextView02("8");
+    }
+    public void push_9 (View veiw){
+        change_in_myTextView02("9");
+    }
+    public void push_0 (View veiw){
+        change_in_myTextView02("0");
+    }
+    public void push_plus (View veiw){
+        change_in_myTextView02("+");
+    }
+    public void push_minus (View veiw){
+        change_in_myTextView02("-");
+    }
+    public void push_multy (View veiw){
+        change_in_myTextView02("*");
+    }
+    public void push_div (View veiw){
+        change_in_myTextView02("-");
     }
 
     public void onClick(View view){
