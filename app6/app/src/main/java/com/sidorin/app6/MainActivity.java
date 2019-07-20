47adbe1425e142c3f5b52e9a7c3e2b4c18708f6b
OlegSidorin <oleg.sidorin@gmal.com>
2019-07-20 17:47:32 +0300

    up to 20.07

---
+++ app6/app/src/main/java/com/sidorin/app6/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sidorin.app6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constrain_layout);
+        // три поля разметка есть текст справа поле для ввода имя и кнопка ввести имя
+        // введите имя поле для ввода кнопка
+        // ниже чек бокс
+        // ниже 2 текста друг над другом и кнопка
+        // привет пока
+    }
+}
