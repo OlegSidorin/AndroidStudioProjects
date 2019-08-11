de90f9205fe1a642da4ecacf9bba40f619ab508a
Oleg Sidorin <oleg.sidorin@gmail.com>
2019-08-11 22:49:29 +0300

    подправил контакт лист

--- ContactList/app/src/main/java/com/sidorin/contactlist/MyAdapter.java
+++ ContactList/app/src/main/java/com/sidorin/contactlist/MyAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,9 +15,11 @@ import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private ArrayList<MyData> data;
+
     public interface OnMyDataEditListener {
         public void OnEditData(ArrayList<MyData> data, int position);
     }
+
     private OnMyDataEditListener onMyDataEditListener;
 
     public MyAdapter(ArrayList<MyData> data) {
@@ -36,6 +39,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         final MyData item = data.get(position);
         holder.tv_name.setText(item.name);
         holder.tv_surname.setText(item.surname);
+        holder.tv_type.setText(item.who);
         holder.btn_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,14 +50,25 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         holder.btn_delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(view.getContext(), String.format("DELETE %s", item.surname), Toast.LENGTH_SHORT).show();
+                Toast.makeText(view.getContext(), String.format("Удален контакт %s", item.surname), Toast.LENGTH_SHORT).show();
                 data.remove(position);
                 // MyAdapter.this.notifyDataSetChanged(); // заново
                 MyAdapter.this.notifyItemRemoved(position);
-                MyAdapter.this.notifyItemRangeChanged(position,data.size()-position);
+                MyAdapter.this.notifyItemRangeChanged(position, data.size() - position);
             }
         });
-       // holder.tv_type.setText(item.type);
+
+        switch (item.gender) {
+            case "f":
+                holder.img_contact.setImageResource(R.drawable.ic_female);
+                break;
+            case "m":
+                holder.img_contact.setImageResource(R.drawable.ic_male);
+                break;
+            default:
+                holder.img_contact.setImageResource(R.drawable.ic_portrait_black_24dp);
+        }
+        // holder.tv_type.setText(item.type);
 
         /*
         switch (item.type) {
@@ -74,6 +89,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView tv_name, tv_surname, tv_type;
         public Button btn_edit, btn_delete;
+        public ImageView img_contact;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -82,6 +98,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             btn_edit = itemView.findViewById(R.id.btn_edit);
             btn_delete = itemView.findViewById(R.id.btn_delete);
             tv_type = itemView.findViewById(R.id.tv_type);
+            img_contact = itemView.findViewById(R.id.iPhoto);
         }
     }
 }
--- ContactList/app/src/main/java/com/sidorin/contactlist/MyData.java
+++ ContactList/app/src/main/java/com/sidorin/contactlist/MyData.java
@@ -6,9 +6,16 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MyData {
-    public String name, surname;
+    public String name, surname, gender, who;
     public int year;
     public int type;
+
+    public MyData(String name, String surname, String gender, String who) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.who = who;
+    }
 
     public MyData(String name, String surname, int year, int type) {
         this.name = name;
@@ -16,7 +23,7 @@ public class MyData {
         this.year = year;
         this.type = type;
     }
-
+/*
     public static ArrayList<MyData> dataGenerator(Context context, int size) {
         ArrayList<MyData> data = new ArrayList<>();
         String[] names = context.getResources().getStringArray(R.array.names);
@@ -27,6 +34,21 @@ public class MyData {
                     surnames[rand.nextInt(names.length)],
                     1990 + rand.nextInt(30),
                     rand.nextInt(4));
+            data.add(item);
+        }
+        return data;
+    }
+    */
+
+    public static ArrayList<MyData> dataGenerator(Context context, int size) {
+        ArrayList<MyData> data = new ArrayList<>();
+        String[] names = context.getResources().getStringArray(R.array.names);
+        String[] surnames = context.getResources().getStringArray(R.array.surnames);
+        String[] gender = context.getResources().getStringArray(R.array.gender);
+        String[] who = context.getResources().getStringArray(R.array.types);
+        Random rand = new Random();
+        for (int i = 0; i < 50; i++) {
+            MyData item = new MyData(names[i], surnames[i], gender[i], who[rand.nextInt(who.length)]);
             data.add(item);
         }
         return data;
