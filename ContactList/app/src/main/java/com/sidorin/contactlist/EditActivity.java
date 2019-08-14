521dc67ff17c8edd8031cb9b1c145a32ba7f9a8f
Oleg Sidorin <oleg.sidorin@gmail.com>
2019-08-14 19:52:30 +0300

    обновил контакт лист

--- ContactList/app/src/main/java/com/sidorin/contactlist/EditActivity.java
+++ ContactList/app/src/main/java/com/sidorin/contactlist/EditActivity.java
@@ -15,10 +15,17 @@ import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
 
+    private String ed_gender;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_contact);
+
+        final EditText ed_name = findViewById(R.id.inputFirstName);
+        final EditText ed_surname = findViewById(R.id.inputSecondName);
+        final Spinner spiner_type = findViewById(R.id.spinner_of_types);
+        final ImageView contactPhotoIV = findViewById(R.id.contactPhoto);
 
         findViewById(R.id.contactPhoto).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,14 +37,21 @@ public class EditActivity extends AppCompatActivity {
         });
 
         Intent intent = getIntent();
-        final EditText ed_name = findViewById(R.id.inputFirstName);
-        final EditText ed_surname = findViewById(R.id.inputSecondName);
-        final Spinner spiner_type = findViewById(R.id.spinner_type);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.types));
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spiner_type.setAdapter(adapter);
         spiner_type.setSelection(3);
+
+        if (intent != null && intent.getExtras() != null) {
+            ed_name.setText(intent.getExtras().getString("name", ""));
+            ed_surname.setText(intent.getExtras().getString("surname", ""));
+            ed_gender = intent.getExtras().getString("gender","");
+            if (ed_gender.equals("f")) contactPhotoIV.setImageResource(R.drawable.ic_female);
+            if (ed_gender.equals("m")) contactPhotoIV.setImageResource(R.drawable.ic_male);
+            spiner_type.setSelection(intent.getExtras().getInt("type", 3));
+        }
 
         findViewById(R.id.btn_makeContact).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +59,7 @@ public class EditActivity extends AppCompatActivity {
                 String name = ed_name.getText().toString();
                 String surname = ed_surname.getText().toString();
                 int type = spiner_type.getSelectedItemPosition();
+                String who;
                 if (name.equals("") || surname.equals("")) {
                     Toast.makeText(EditActivity.this, "no data", Toast.LENGTH_SHORT).show();
                 }
@@ -52,15 +67,13 @@ public class EditActivity extends AppCompatActivity {
                 intent.putExtra("name", name);
                 intent.putExtra("surname", surname);
                 intent.putExtra("type", type);
+                who = spiner_type.getSelectedItem().toString();
+                intent.putExtra("who", who);
                 intent.putExtra("position", getIntent().getExtras().getInt("position"));
                 setResult(RESULT_OK, intent);
                 EditActivity.this.finish();
 
-                if (intent != null && intent.getExtras() != null) {
-                    ed_name.setText(intent.getExtras().getString("name", ""));
-                    ed_surname.setText(intent.getExtras().getString("surname", ""));
-                    spiner_type.setSelection(intent.getExtras().getInt("type", 3));
-                }
+
             }
 
         });
--- ContactList/app/src/main/java/com/sidorin/contactlist/MyData.java
+++ ContactList/app/src/main/java/com/sidorin/contactlist/MyData.java
@@ -10,11 +10,12 @@ public class MyData {
     public int year;
     public int type;
 
-    public MyData(String name, String surname, String gender, String who) {
+    public MyData(String name, String surname, String gender, String who, int type) {
         this.name = name;
         this.surname = surname;
         this.gender = gender;
         this.who = who;
+        this.type = type;
     }
 
     public MyData(String name, String surname, int year, int type) {
@@ -40,15 +41,17 @@ public class MyData {
     }
     */
 
-    public static ArrayList<MyData> dataGenerator(Context context, int size) {
+    public static ArrayList<MyData> dataGenerator(Context context) {
         ArrayList<MyData> data = new ArrayList<>();
         String[] names = context.getResources().getStringArray(R.array.names);
         String[] surnames = context.getResources().getStringArray(R.array.surnames);
         String[] gender = context.getResources().getStringArray(R.array.gender);
         String[] who = context.getResources().getStringArray(R.array.types);
+        int[] type = new int[context.getResources().getStringArray(R.array.names).length];
         Random rand = new Random();
         for (int i = 0; i < 50; i++) {
-            MyData item = new MyData(names[i], surnames[i], gender[i], who[rand.nextInt(who.length)]);
+            type[i] = rand.nextInt(who.length);
+            MyData item = new MyData(names[i], surnames[i], gender[i], who[type[i]], type[i]);
             data.add(item);
         }
         return data;
